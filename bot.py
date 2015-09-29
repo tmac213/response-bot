2288ae1caf437518f94d12a4048bfe4a4c69a149
Emilio Colindres <exc231@case.edu>
2015-09-28 22:29:35 -0400

    respond to Noemi

--- bot.py
+++ bot.py
@@ -1,6 +1,28 @@
+#!/usr/bin/env python3
+from pprint import pprint
 from secrets import *
 from twitter import *
 
-t = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, API_KEY, 
-API_SECRET))
+USERNAME = 'emilio___'
 
+def main():
+    twitterObject = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, API_KEY, API_SECRET))
+    twitterStream = TwitterStream(domain='userstream.twitter.com', auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, API_KEY, API_SECRET))
+    
+    while True:
+        stream = twitterStream.user()
+    
+        for tweet in stream:
+            pprint(tweet)
+        
+            if 'event' in tweet:
+                print('received event %s' % tweet['event'])
+            elif 'hangup' in tweet:
+                return
+            elif 'text' in tweet and tweet['user']['screen_name'] == '_mimi25':
+                print('from @%s: %s' % (tweet['user']['screen_name'], tweet['text']))
+                print('responding with I love you')
+                reply = '@%s %s' % (tweet['user']['screen_name'], 'I love you')
+                twitterObject.statuses.update(status=reply, in_reply_to_status_id=tweet['id'])
+
+if __name__ == "__main__": main()
